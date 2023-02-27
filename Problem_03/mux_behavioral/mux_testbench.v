f21960d292e795e7264046333ae3a269584dbc2d
Suriya2882002 <112601117+Suriya2882002@users.noreply.github.com>
2023-02-27 12:03:58 +0530

    Add files via upload

---
+++ Problem_03/mux_behavioral/mux_testbench.v
@@ -0,0 +1,16 @@
+module mux_testbench;
+reg a,c,sel;
+wire b;
+mux_behavioral mux_test(a,c,sel,b);      
+initial
+begin
+sel=0;a=0;c=0;
+#100 sel=0;a=0;c=1;
+#100 sel=0;a=1;c=0;
+#100 sel=0;a=1;c=1;
+#100 sel=1;a=0;c=0;
+#100 sel=1;a=0;c=1;
+#100 sel=1;a=1;c=0;
+#100 sel=1;a=1;c=1;
+end
+endmodule
